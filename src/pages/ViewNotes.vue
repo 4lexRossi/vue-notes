c4a023c4c50eaa266ae8461b9f802eecdb1d1827
4lexRossi <devalexrossi@gmail.com>
2022-07-04 20:05:50 -0300

    install bulma and customize note page

--- src/pages/ViewNotes.vue
+++ src/pages/ViewNotes.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new Note" />
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Reprehenderit harum voluptas libero ad ducimus autem ex, distinctio
+          eveniet eum perspiciatis adipisci possimus exercitationem repudiandae
+          alias nisi nulla voluptatibus quam mollitia.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
