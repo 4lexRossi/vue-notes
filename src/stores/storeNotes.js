945f9f05324bea722faf0e92ab414a414f9e7133
4lexRossi <devalexrossi@gmail.com>
2022-07-27 15:30:51 -0300

    connect delete and update note to firestore

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,5 +1,12 @@
 import { defineStore } from 'pinia';
-import { collection, onSnapshot, doc, setDoc } from 'firebase/firestore';
+import {
+  collection,
+  onSnapshot,
+  doc,
+  setDoc,
+  deleteDoc,
+  updateDoc,
+} from 'firebase/firestore';
 import { db } from '@/js/firebase';
 
 const notesCollectionRef = collection(db, 'notes');
@@ -28,22 +35,17 @@ export const useStoreNotes = defineStore('storeNotes', {
     async addNote(newNoteContent) {
       const currentDate = new Date().getTime() + Math.random(2),
         id = currentDate.toString();
-      // const note = {
-      //   id,
-      //   content: newNoteContent,
-      // };
-      // this.notes.unshift(note);
       await setDoc(doc(notesCollectionRef, id), {
-        content: newNoteContent
+        content: newNoteContent,
+      });
+    },
+    async deleteNote(id) {
+      await deleteDoc(doc(notesCollectionRef, id));
+    },
+    async updateNote(id, content) {
+      await updateDoc(doc(notesCollectionRef, id), {
+        content
       })
-    },
-    deleteNote(id) {
-      this.notes = this.notes.filter((f) => f.id !== id);
-    },
-    updateNote(id, content) {
-      //doing without return and curly bracketes
-      const index = this.notes.findIndex((note) => note.id === id);
-      this.notes[index].content = content;
     },
   },
   getters: {
