aaa77d0a53690640ea9bfa8405bacb000ad5fdfd
4lexRossi <devalexrossi@gmail.com>
2022-07-27 14:59:23 -0300

    remove config file

--- src/js/firebaseMock.js
+++ src/js/firebaseMock.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAHwbbUW0RFVxMVU22vk7Kyy4EL1jcsMeU',
-  authDomain: 'vue-notes-2a3d8.firebaseapp.com',
-  projectId: 'vue-notes-2a3d8',
-  storageBucket: 'vue-notes-2a3d8.appspot.com',
-  messagingSenderId: '133340578106',
-  appId: '1:133340578106:web:db5f3bc60d0887b5699003',
+  apiKey: '<Your apiKey>',
+  authDomain: '<Your authDomain>',
+  projectId: '<Your projectId>',
+  storageBucket: '<Your storageBucket>',
+  messagingSenderId: '<Your messagingSenderId>',
+  appId: '<Your appId>',
 };
 
 // Initialize Firebase
