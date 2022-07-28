d6cf48de920706dd54590d003dd94786f9aa8ad5
4lexRossi <devalexrossi@gmail.com>
2022-07-28 18:18:41 -0300

    Created register and logout functions

--- src/stores/storeAuth.js
+++ src/stores/storeAuth.js
@@ -1,9 +1,28 @@
 import { defineStore } from 'pinia';
-import { auth } from '@/js/firebase'
+import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '@/js/firebase';
 
 export const useStoreAuth = defineStore('storeAuth', {
   state: () => {
     return {};
   },
-  actions: {},
+  actions: {
+    registerUser(credentials) {
+      createUserWithEmailAndPassword(auth, credentials.email, credentials.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+        }).catch((error) => {
+          const errorMessage = error.message;
+          console.log(errorMessage)
+        });
+    },
+    logoutUser() {
+      signOut(auth).then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+    }
+  },
 });
